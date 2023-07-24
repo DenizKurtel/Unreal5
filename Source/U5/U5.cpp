722ce5ed0d8246732c176a4cc07e7549f39b371b
DenizKurtel <deniz.kurtel@gmail.com>
2023-07-24 21:23:52 +0100

    Initial commit

---
+++ Source/U5/U5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "U5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, U5, "U5" );
---
+++ Source/U5/U5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "U5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U5_API AU5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
